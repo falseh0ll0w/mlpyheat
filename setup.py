08f5730c7737dcc6764f74956316491bd4c88d4a
h0_0w <146596580+falseh0ll0w@users.noreply.github.com>
2024-01-06 07:16:35 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
-setup(
-    name="<YOUR_PROJECT_NAME>",
+setup( --install 
+    name="<mlpyheat>",
     version="1.0",
     packages=find_packages(),
 )
